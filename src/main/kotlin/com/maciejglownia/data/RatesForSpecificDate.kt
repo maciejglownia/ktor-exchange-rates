fac33655d17de98cee5f226e024b342b39fc907f
Maciej Glownia <maciej.glownia@infor.com>
2022-12-15 14:26:11 +0100

    Add method which returns Map<String, Double> of rates for specific date

---
+++ src/main/kotlin/com/maciejglownia/data/RatesForSpecificDate.kt
@@ -0,0 +1,30 @@
+package com.maciejglownia.data
+
+fun getRatesFor20221215(): Map<String, Double> {
+    return mapOf(
+        "AAA" to 0.01,
+        "BBB" to 0.02,
+        "CCC" to 0.03,
+        "DDD" to 0.04,
+        "EUR" to 1.00,
+        "FFF" to 1.06,
+        "GGG" to 1.07,
+        "HHH" to 1.08,
+        "III" to 1.09,
+        "JJJ" to 1.10,
+        "KKK" to 1.11,
+        "LLL" to 1.12,
+        "MMM" to 1.13,
+        "NNN" to 1.14,
+        "OOO" to 1.15,
+        "PPP" to 1.16,
+        "RRR" to 1.17,
+        "SSS" to 1.18,
+        "TTT" to 1.19,
+        "UUU" to 1.20,
+        "WWW" to 1.21,
+        "XXX" to 1.22,
+        "YYY" to 1.23,
+        "ZZZ" to 1.24,
+    )
+}
